1f4c9adb4064ea567cc9d760a26ad74fe9840532
devarathinam <devarathinambdr@gmail.com>
2021-10-05 21:51:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const http = require('http').createServer(app);
+
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+
+
+
+const port = process.env.PORT || 8080;
+http.listen(port, () => {
+    console.log("Node JS is running on port 8080")
+})
