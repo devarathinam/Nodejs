13577ed3d3b6103ad2293396f4db16a5737837c0
devarathinam <devarathinambdr@gmail.com>
2021-10-11 18:17:50 +0530

    Mysql connection with apache server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-
+const mysql = require('mysql');
 const app = express();
 const http = require('http').createServer(app);
 
@@ -9,74 +8,38 @@ const http = require('http').createServer(app);
 app.use(bodyParser.json()); 
 app.use(express.json());
 
-const userList = [
-    {
-      name : "Deva",
-      age : 20,
-      id : 1
-    },
-    {
-      name : "Rathinam",
-      age : 28,
-      id : 2
-    }
-    
-  ]
+const connection = mysql.createConnection({
+  host : "localhost",
+  user : "root",
+  password : ""
+})
+connection.connect((error) => {
+  if(error){
+    throw error;
+  }
+
+  console.log("MYSQL Database is sucessfully connected!!!")
+
+});
   
   app.get("/api/user/list", (request, response) => {
-    response.status(200).send(userList);
+    
   });
 
   app.post("/api/user/create", (request, response) => {
-    let user = request.body;
-    user.id = userList.length + 1;
-  
-    userList.push(user);
-    response.status(200).send({
-      message : "Successfully created the user profile"
-    })
+    
   });
   
   app.put("/api/user/edit/:id", (request, response) => {
-    let id = request.params.id;
-    let index = userList.findIndex((value) => {
-      return value.id == id;
-    });
-  
-    if(index < 0){
-      response.status(401).send({
-        message : "Invalid ID number"
-      });
-      return;
-    }
-  
-    userList[index].name = request.body.name;
-    userList[index].age = request.body.age;
+     
     
-    response.status(200).send({
-      message : "Successfully Updated the user information"
-    })
   
   });
   
   
   app.delete("/api/user/delete/:id", (request, response) => {
-    let id = request.params.id;
-    let index = userList.findIndex((value) => {
-      return value.id == id;
-    });
-  
-    if(index < 0){
-      response.status(401).send({
-        message : "Invalid ID number"
-      });
-      return;
-    }
-  
-    userList.splice(index, 1);
-    response.status(200).send({
-      message : "User profile has been deleted successfully"
-    })
+    
+   
   });
 
 
